d8c43d6081ec383f69ad0ca7ffb9c94eb239826a
camolezi <camolezi@usp.br>
2020-09-24 21:39:00 -0300

    Added react-router, create route switching in the main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route exact path="/home">
+          <h1>HOME</h1>
+        </Route>
+        <Route exact path="/login">
+          LOGIN
+        </Route>
+        <Route exact path="/signup">
+          SIGNUP
+        </Route>
+        <Route>404 not found</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
